c9c045e92170308b68a258da0304bba75b215901
Cliff-Tech58 <cliffscoff58@gmail.com>
2025-02-22 13:27:25 +0300

    data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 // Hardcoded participants data
 const participants = [
-    { name: "Emmanuel", hasPaid: false, gameWeek: "GWK 26", points: 14 },
-    { name: "Bethuel", hasPaid: true, gameWeek: "GWK 26", points:  0 },   // Bethuel has paid
-    { name: "David", hasPaid: true, gameWeek: "GWK 26", points:  0},
-    { name: "Clinton", hasPaid: true, gameWeek: "GWK 26", points:  8 },   // Clinton has paid
-    { name: "Scoff", hasPaid: true, gameWeek: "GWK 26", points:  14 },     // Scoff has paid
-    { name: "Michael", hasPaid: true, gameWeek: "GWK 26", points:  0 },   // Michael has paid
-    { name: "Steve", hasPaid: true, gameWeek: "GWK 26", points: 10},     // Steve has paid
-    { name: "Sam", hasPaid: false, gameWeek: "GWK 26", points:  22 },
-    { name: "Alex", hasPaid: true, gameWeek: "GWK 26", points:  21 },
-  ];
+  { name: "Emmanuel", hasPaid: false, gameWeek: "GWK 26", points: 75 },
+  { name: "Bethuel", hasPaid: true, gameWeek: "GWK 26", points: 85 },   // Bethuel has paid
+  { name: "David", hasPaid: false, gameWeek: "GWK 26", points: 60 },
+  { name: "Clinton", hasPaid: true, gameWeek: "GWK 26", points: 95 },   // Clinton has paid
+  { name: "Scoff", hasPaid: true, gameWeek: "GWK 26", points: 90 },     // Scoff has paid
+  { name: "Michael", hasPaid: true, gameWeek: "GWK 26", points: 80 },   // Michael has paid
+  { name: "Steve", hasPaid: true, gameWeek: "GWK 26", points: 88 },     // Steve has paid
+  { name: "Sam", hasPaid: false, gameWeek: "GWK 26", points: 70 },
+  { name: "Alex", hasPaid: false, gameWeek: "GWK 26", points: 65 },
+];
