afcb3cfda90dbd4f4a772b43ba295716db2138ba
Cliff-Tech58 <cliffscoff58@gmail.com>
2025-02-22 13:31:45 +0300

    data.js

--- data.js
+++ data.js
@@ -2,11 +2,11 @@
 const participants = [
   { name: "Emmanuel", hasPaid: false, gameWeek: "GWK 26", points: 75 },
   { name: "Bethuel", hasPaid: true, gameWeek: "GWK 26", points: 85 },   // Bethuel has paid
-  { name: "David", hasPaid: false, gameWeek: "GWK 26", points: 60 },
+  { name: "David", hasPaid: true, gameWeek: "GWK 26", points: 60 },
   { name: "Clinton", hasPaid: true, gameWeek: "GWK 26", points: 95 },   // Clinton has paid
   { name: "Scoff", hasPaid: true, gameWeek: "GWK 26", points: 90 },     // Scoff has paid
   { name: "Michael", hasPaid: true, gameWeek: "GWK 26", points: 80 },   // Michael has paid
   { name: "Steve", hasPaid: true, gameWeek: "GWK 26", points: 88 },     // Steve has paid
   { name: "Sam", hasPaid: false, gameWeek: "GWK 26", points: 70 },
-  { name: "Alex", hasPaid: false, gameWeek: "GWK 26", points: 65 },
+  { name: "Alex", hasPaid: true, gameWeek: "GWK 26", points: 65 },
 ];
