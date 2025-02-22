a6c2040e86660ff1af95693c2edd9f26e05cea21
Cliff-Tech58 <cliffscoff58@gmail.com>
2025-02-22 14:00:15 +0300

    data.js

--- data.js
+++ data.js
@@ -7,6 +7,6 @@ const participants = [
     { name: "Scoff", hasPaid: true, gameWeek: "GWK 26", points:  14 },     // Scoff has paid
     { name: "Michael", hasPaid: true, gameWeek: "GWK 26", points:  0 },   // Michael has paid
     { name: "Steve", hasPaid: true, gameWeek: "GWK 26", points: 10},     // Steve has paid
-    { name: "Sam", hasPaid: false, gameWeek: "GWK 26", points:  22 },
+    { name: "Sam", hasPaid: true, gameWeek: "GWK 26", points:  22 },
     { name: "Alex", hasPaid: true, gameWeek: "GWK 26", points:  21 },
   ];
